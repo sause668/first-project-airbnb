f7e274f6e69b2f2254a38bb9ff986207569d7a3b
sause668 <soulsitescoding@zohomail.com>
2024-07-24 22:56:44 -0400

    Finished up to Add Spot Image

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const pagination = require('../../utils/pagination');
+const { requireAuth }  = require('../../utils/auth')
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('sqlite::memory:');
 
 // Import model(s)
-const { Spot, Review, SpotImage } = require('../../db/models');
+const { Spot, Review, SpotImage, User } = require('../../db/models');
 const { Op } = require('sequelize');
 
 // Returns all the spots
@@ -14,10 +15,12 @@ router.get('/', async (req, res, next) => {
         subQuery: false,
         attributes: {
             include: [
+                // Set Average Rating
                 [
                     Sequelize.fn('AVG', Sequelize.col('Reviews.stars')),
                     'avgRating'
                 ],
+                //Set preview Image
                 [
                     sequelize.literal(`(
                         SELECT SpotImages.url
@@ -32,12 +35,14 @@ router.get('/', async (req, res, next) => {
                 ],
             ]
         },
+        // Required for avgRating
         include: {
             model: Review,
             required: false,
             attributes: []
         },
         group: ['Spot.id'],
+        // For future queries
         // where
         // order: [['name', 'ASC']],
         // limit,
@@ -49,13 +54,106 @@ router.get('/', async (req, res, next) => {
 });
 
 // Returns all the spots owned by the current user.
-router.get('/current', async (req, res, next) => {
+router.get('/current', requireAuth, async (req, res, next) => {
+    const { user } = req;
+    const where = {};
+
+    //Set restraint to current user
+    where.ownerId = user.id;
     
+    const spotsUser = await Spot.findAll({
+        subQuery: false,
+        attributes: {
+            include: [
+                // Set avgRating
+                [
+                    Sequelize.fn('AVG', Sequelize.col('Reviews.stars')),
+                    'avgRating'
+                ],
+                // Set previewImage
+                [
+                    sequelize.literal(`(
+                        SELECT SpotImages.url
+                        FROM SpotImages
+                        JOIN Spots ON Spots.id = SpotImages.spotId
+                        WHERE
+                            SpotImages.spotId = Spots.id
+                            AND
+                            SpotImages.preview = true
+                    )`),
+                    'previewImage',
+                ],
+            ]
+        },
+        // Required for avgRating
+        include: {
+            model: Review,
+            required: false,
+            attributes: []
+        },
+        group: ['Spot.id'],
+        where
+        // For future queries
+        // order: [['name', 'ASC']],
+        // limit,
+        // offset
+    });
+
+    res.json(spotsUser);
 });
 
 // Returns the details of a spot specified by its id.
 router.get('/:spotId', async (req, res, next) => {
+    const { spotId } = req.params;
+    const where = {};
+
+    //Set restraint to specified ID
+    where.id = parseInt(spotId);
     
+    const spot = await Spot.findAll({
+        subQuery: false,
+        attributes: {
+            include: [
+                // Set numReviews
+                [
+                    Sequelize.fn('COUNT', Sequelize.col('Reviews.stars')),
+                    'numReviews'
+                ],
+                // Set avgRating
+                [
+                    Sequelize.fn('AVG', Sequelize.col('Reviews.stars')),
+                    'avgRating'
+                ]
+            ]
+        },
+        include: [
+            // Required for numReviews and avgRating
+            {
+                model: Review,
+                required: false,
+                attributes: []
+            },
+            // Adds SpotImages
+            {
+                model: SpotImage,
+                attributes: ['id', 'url', 'preview']
+            },
+            // Adds Owner
+            {
+                model: User,
+                as: 'Owner',
+                attributes: ['id', 'firstName', 'lastName']
+            }
+        ],
+        group: ['Spot.id'],
+        where
+        // For future queries
+        // order: [['name', 'ASC']],
+        // limit,
+        // offset
+    });
+
+    res.json(spot);
 });
 
 // Return all the bookings for a spot specified by id.
@@ -72,8 +170,27 @@ router.get('/:spotId/reviews', async (req, res, next) => {
 
 
 // Creates and returns a new spot.
-router.post('/', async (req, res, next) => {
-    
+router.post('/', requireAuth, async (req, res, next) => {
+    const { user } = req;
+
+    // Deconstruct variables form body
+    const { address, city, state, country, lat, lng, name, description, price} = req.body;
+
+    // Create new spot using userId and body variables
+    const spotNew = await Spot.create({
+        ownerId: user.id,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+    });
+
+    res.json(spotNew);
 });
 
 // Create and return a new booking from a spot specified by id.
@@ -87,8 +204,24 @@ router.post('/:spotId/reviews', async (req, res, next) => {
 });
 
 // Create and return a new image for a spot specified by id.
-router.post('/:spotId/images', async (req, res, next) => {
-    
+router.post('/:spotId/images', requireAuth, async (req, res, next) => {
+    const { spotId } = req.params;
+    const { url, preview} = req.body;
+
+    const spot = await Spot.findOne({where: {id: spotId}});
+
+    const spotImageNew = await spot.createSpotImage({
+        url,
+        preview
+    });
+
+    const safeImage = {
+        id: spotImageNew.id,
+        url: spotImageNew.url,
+        preview: spotImageNew.preview
+    }
+
+    res.json(safeImage);
 });
 
 
