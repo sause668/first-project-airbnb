b828f8a4b12b3f082e0661a5e546c26259a9e492
sause668 <soulsitescoding@zohomail.com>
2024-08-01 22:42:57 -0400

    Debug

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -43,7 +43,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         validate: {
           notEmpty: {
-              args: [true],
+              args: true,
               msg: 'Last Name is required'
           }
         }
