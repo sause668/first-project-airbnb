fe39e46d39be989d1d5de5c57c6340283096574f
sause668 <soulsitescoding@zohomail.com>
2024-08-01 23:40:29 -0400

    Debug

--- backend/db/migrations/20240723020222-create-spot.js
+++ backend/db/migrations/20240723020222-create-spot.js
@@ -9,99 +9,99 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // await queryInterface.createTable('Spots', {
-    //   id: {
-    //     allowNull: false,
-    //     autoIncrement: true,
-    //     primaryKey: true,
-    //     type: Sequelize.INTEGER
-    //   },
-    //   ownerId: {
-    //     type: Sequelize.INTEGER,
-    //     allowNull: false,
-    //     references: {
-    //       model: 'Users',
-    //       key: 'id',
-    //     },
-    //     onDelete: 'cascade'
-    //   },
-    //   address: {
-    //     type: Sequelize.STRING,
-    //     allowNull: false,
-    //     validate: {
-    //       len: [5, 100]
-    //     }
-    //   },
-    //   city: {
-    //     type: Sequelize.STRING,
-    //     allowNull: false,
-    //     validate: {
-    //       len: [2, 50]
-    //     }
-    //   },
-    //   state: {
-    //     type: Sequelize.STRING,
-    //     allowNull: false,
-    //     validate: {
-    //       len: [2, 50]
-    //     }
-    //   },
-    //   country: {
-    //     type: Sequelize.STRING,
-    //     allowNull: false,
-    //     validate: {
-    //       len: [2, 50]
-    //     }
-    //   },
-    //   lat: {
-    //     type: {
-    //       type: Sequelize.DECIMAL,
-    //       validate: {
-    //         min: -90,
-    //         max: 90
-    //       }
-    //     }
-    //   },
-    //   lng: {
-    //     type: {
-    //       type: Sequelize.DECIMAL,
-    //       validate: {
-    //         min: -180,
-    //         max: 180
-    //       }
-    //     }
-    //   },
-    //   name: {
-    //     type: Sequelize.STRING,
-    //     allowNull: false,
-    //     validate: {
-    //       len: [3, 50]
-    //     }
-    //   },
-    //   description: {
-    //     type: Sequelize.TEXT,
-    //     allowNull: false
-    //   },
-    //   price: {
-    //     type: Sequelize.DECIMAL(10,2),
-    //     allowNull: false,
-    //     validate: {
-    //       min: 0
-    //     }
-    //   },
-    //   createdAt: {
-    //     allowNull: false,
-    //     type: Sequelize.DATE,
-    //     defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-    //   },
-    //   updatedAt: {
-    //     allowNull: false,
-    //     type: Sequelize.DATE,
-    //     defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-    //   }
-    // }, options);
+    await queryInterface.createTable('Spots', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      // ownerId: {
+      //   type: Sequelize.INTEGER,
+      //   allowNull: false,
+      //   references: {
+      //     model: 'Users',
+      //     key: 'id',
+      //   },
+      //   onDelete: 'cascade'
+      // },
+      // address: {
+      //   type: Sequelize.STRING,
+      //   allowNull: false,
+      //   validate: {
+      //     len: [5, 100]
+      //   }
+      // },
+      // city: {
+      //   type: Sequelize.STRING,
+      //   allowNull: false,
+      //   validate: {
+      //     len: [2, 50]
+      //   }
+      // },
+      // state: {
+      //   type: Sequelize.STRING,
+      //   allowNull: false,
+      //   validate: {
+      //     len: [2, 50]
+      //   }
+      // },
+      // country: {
+      //   type: Sequelize.STRING,
+      //   allowNull: false,
+      //   validate: {
+      //     len: [2, 50]
+      //   }
+      // },
+      // lat: {
+      //   type: {
+      //     type: Sequelize.DECIMAL,
+      //     validate: {
+      //       min: -90,
+      //       max: 90
+      //     }
+      //   }
+      // },
+      // lng: {
+      //   type: {
+      //     type: Sequelize.DECIMAL,
+      //     validate: {
+      //       min: -180,
+      //       max: 180
+      //     }
+      //   }
+      // },
+      // name: {
+      //   type: Sequelize.STRING,
+      //   allowNull: false,
+      //   validate: {
+      //     len: [3, 50]
+      //   }
+      // },
+      // description: {
+      //   type: Sequelize.TEXT,
+      //   allowNull: false
+      // },
+      // price: {
+      //   type: Sequelize.DECIMAL(10, 2),
+      //   allowNull: false,
+      //   validate: {
+      //     min: 0
+      //   }
+      // },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    // await queryInterface.dropTable('Spots');
+    await queryInterface.dropTable('Spots');
   }
 };
