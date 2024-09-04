c2274a9999f2093ae1fc996f54a72ef94826d73e
sause668 <soulsitescoding@zohomail.com>
2024-09-03 20:57:36 -0400

    Finished Outline

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,42 +1,66 @@
+// import { NavLink } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import ProfileButton from './ProfileButton';
+// import OpenModalButton from '../OpenModalButton/OpenModalButton';
+// import LoginFormModal from '../LoginFormModal/LoginFormModal';
+// import SignupFormModal from '../SignupFormModal/SignupFormModel';
+// import './Navigation.css';
+
+// function Navigation({ isLoaded }) {
+//   const sessionUser = useSelector((state) => state.session.user);
+
+//   let sessionLinks = [];
+//   if (sessionUser) {
+//     sessionLinks.push((
+//         <ProfileButton user={sessionUser} />
+//     ));
+//   } else {
+//     sessionLinks.push(
+//         (
+//             <OpenModalButton
+//             buttonText="Log In"
+//             modalComponent={<LoginFormModal />}
+//             />
+//         ),
+//         (
+//             <OpenModalButton
+//             buttonText="Sign Up"
+//             modalComponent={<SignupFormModal />}
+//             />
+//         )
+//     );
+//   }
+
+//   return (
+//     <ul>
+//       <li>
+//         <NavLink to="/">Home</NavLink>
+//       </li>
+//       {isLoaded && sessionLinks.map((link, index) => (<li key={`sessionLink${index}`}>{link}</li>))}
+//     </ul>
+//   );
+// }
+
+// export default Navigation;
+
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import OpenModalButton from '../OpenModalButton/OpenModalButton';
-import LoginFormModal from '../LoginFormModal/LoginFormModal';
-import SignupFormModal from '../SignupFormModal/SignupFormModel';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
-  const sessionUser = useSelector((state) => state.session.user);
-
-  let sessionLinks = [];
-  if (sessionUser) {
-    sessionLinks.push((
-        <ProfileButton user={sessionUser} />
-    ));
-  } else {
-    sessionLinks.push(
-        (
-            <OpenModalButton
-            buttonText="Log In"
-            modalComponent={<LoginFormModal />}
-            />
-        ),
-        (
-            <OpenModalButton
-            buttonText="Sign Up"
-            modalComponent={<SignupFormModal />}
-            />
-        )
-    );
-  }
+  const sessionUser = useSelector(state => state.session.user);
 
   return (
     <ul>
       <li>
         <NavLink to="/">Home</NavLink>
       </li>
-      {isLoaded && sessionLinks.map((link, index) => (<li key={`sessionLink${index}`}>{link}</li>))}
+      {isLoaded && (
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
     </ul>
   );
 }
